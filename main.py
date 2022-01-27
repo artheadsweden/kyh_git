01b0f52badeb7812a6ce1da46d17dcec45dd36d9
Joakim Kyh <joakim.kyh@arthead.se>
2022-01-27 11:25:02 +0100

    F2_Merge_Conflict_Solved

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import requests
 
 def main():
     result = requests.get('https://svt.se')
+
     title_part1 = result.text.split('<title data-react-helmet="true">')[1]
     title = title_part1.split('</title>')[0]
     print('title:', title)
+
+    print('number of <p>-tags:', result.text.count('<p'))
 
 
 if __name__ == '__main__':
