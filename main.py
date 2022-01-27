57a7b713aef81e04938a796ca08d8eb3a6c1e2d9
Joakim Wassberg <joakim@arthead.se>
2022-01-27 11:12:52 +0100

    F1_2

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     result = requests.get('https://svt.se')
     title_part1 = result.text.split('<title data-react-helmet="true">')[1]
     title = title_part1.split('</title>')[0]
-    print(title)
+    print('title:', title)
 
 
 if __name__ == '__main__':
