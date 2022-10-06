9645edec481a564a51bf3cf2745c3ae369fb7675
unknown <joakim.kyh@arthead.se>
2022-10-06 09:31:46 +0200

    Changed

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     title_part1 = result.text.split('<title data-react-helmet="true">')[1]
     title = title_part1.split('</title>')[0]
     print('title:', title)
-
+    # Hej
     print('number of <p>-tags:', result.text.count('<p'))
 
 
